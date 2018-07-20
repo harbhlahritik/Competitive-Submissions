10fca2b1d7ff5c8aead3a3741e99816fc5e34fef
harbhlahritik <harbhlahritik@gmail.com>
2018-07-20 12:58:32 +0530

    Added Code

---
+++ leetcode/reverseString.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String reverseString(String s) {
+        StringBuilder str = new StringBuilder();
+        for(int i = s.length()-1;i >= 0;i--){
+            str.append(s.charAt(i));
+        }
+        String ans = str.toString();
+        return ans;
+    }
+}
